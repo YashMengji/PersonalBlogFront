74469c4afca666d9278973e116e4613f49db859b
YashMengji <yashmengji2172005@gmail.com>
2024-11-28 23:42:33 +0530

    setup basic configuration and industry standards

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import App from './App'
+import Home from '../components/Home'
+import Login from '../components/Login'
+import Register from '../components/Register'
+import ProtectedRoute from '../components/ProtectedRoute'
+
+const createRouter = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App/>,
+      children: [ 
+        {
+          path: "/register", 
+          element:<Register/>
+        }, 
+        {
+          path: "/login", 
+          element: <Login/>
+        }, 
+        {
+          path: '/home',
+          element: (
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          ),
+        },
+       
+      ]
+    }
+  ]
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={createRouter}/>
   </StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from "dotenv"
+dotenv.config();
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.VITE_SERVER_URL, //URL of backend
+        changeOrigin: true, //Changes the origin of the host header to the target URL
+      }
+    },
+  },
   plugins: [react()],
 })
